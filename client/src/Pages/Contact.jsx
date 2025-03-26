5e048ef26c91809bf1cf532964a9e65122aefd17
Akuafo-Patawah12 <burxells873@gmail.com>
2025-03-26 18:31:32 +0000

    update

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -1,4 +1,6 @@
 import React,{useState} from "react";
+import { Facebook, MessageCircle } from "lucide-react";
+
 
 const ContactPage = () => {
 
@@ -46,6 +48,8 @@ const ContactPage = () => {
 
         
       };
+
+      
 
 
   return (
@@ -99,9 +103,28 @@ const ContactPage = () => {
             {loading ? "Sending..." : "Send Message"}
           </button>
         </form>
-        <div className="mt-6 text-center text-gray-600">
-          <p>Email: burxells873@gmail.com</p>
+        
+        <div className="mt-3 mx-auto flex gap-3 w-fit">
+      <a href="https://facebook.com/" target="_blank" rel="noopener noreferrer">
+        <Facebook className="w-6 h-6 text-blue-600 hover:text-blue-800" />
+      </a>
+
+      <a href="https://wa.me/1234567890" target="_blank" rel="noopener noreferrer">
+      <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className="w-6 h-6 text-green-500 hover:text-green-700"
+  >
+    <path d="M12 2a10 10 0 0 0-10 10 10 10 0 0 0 1.5 5.2L2 22l5.3-1.5A10 10 0 0 0 22 12 10 10 0 0 0 12 2zm4.5 13.8c-.3.8-1.5 1.5-2.1 1.4-.6-.1-1.3-.5-2.8-1.3-2.3-1.3-3.8-3-4.1-3.3-.4-.5-.9-1.2-.9-2s.3-1.4.6-1.9c.3-.5.7-.6.9-.6h.7c.3 0 .5 0 .7.5.3.6.9 2 .9 2.1s.2.3 0 .6c-.2.3-.4.5-.6.7s-.3.4-.1.8c.2.3.8 1.4 1.7 2.2 1.2 1 2.1 1.3 2.4 1.4.3.1.5 0 .7-.3.2-.3.8-.9.9-1.1.1-.2.2-.2.4-.1.2.1 1 .5 1.2.6.2.1.4.2.5.3s.2.3.2.4c0 .2-.3 1.2-.7 1.9z" />
+  </svg>
+      </a>
+    </div>
+
+    <div className="mt-3 text-center text-gray-600">
           <p>Phone: +233201623251</p>
+          <p>Email: burxells873@gmail.com</p>
+          
         </div>
       </div>
       {responseMessage && (
