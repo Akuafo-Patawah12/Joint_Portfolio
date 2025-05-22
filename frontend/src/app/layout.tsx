c220f4b08881e8cfc198186757cd7f1e4bb93a5b
Akuafo-Patawah12 <burxells873@gmail.com>
2025-05-22 22:15:41 +0000

    update

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import Head from "next/head"
+
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "./components/Header"
 import Footer from "./components/Footer"
