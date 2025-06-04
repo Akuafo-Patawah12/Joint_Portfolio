f7ba39be980ee9880208d76f5a61eb38ef5e0021
Akuafo-Patawah12 <burxells873@gmail.com>
2025-06-04 15:56:17 +0000

    update

--- frontend/src/app/components/NavModal.tsx
+++ frontend/src/app/components/NavModal.tsx
@@ -18,28 +18,29 @@ const NavModal = () => {
      };
     return(
         <div
-        className={`fixed top-[80px] left-1/2 -translate-1/2 py-10 w-64 bg-white shadow-md z-40 transform ${
-          isSidebarCollapsed ? 'translate-x-0' : '-translate-x-full'
-        } transition-transform duration-300 ease-in-out md:hidden`}
-      >
-        <div className="flex items-center justify-between p-4 border-b">
-          <h2 className="text-lg font-semibold">Menu</h2>
-          <button onClick={() => toggleSidebar()}>
-            <XIcon className="w-5 h-5" />
-          </button>
-        </div>
-        <ul className="flex flex-col gap-4 p-4">
-          <li>
-            <Link href="/" onClick={() => toggleSidebar()}>Home</Link>
-          </li>
-          <li>
-            <Link href="/about" onClick={() => toggleSidebar()}>About</Link>
-          </li>
-          <li>
-            <Link href="/contact" onClick={() => toggleSidebar()}>Contact</Link>
-          </li>
-        </ul>
-      </div>
+  className={`fixed top-[100px] left-0 px-[2.5%] w-full bg-white shadow-md z-40 transform transition-transform duration-300 ease-in-out md:hidden ${
+    isSidebarCollapsed ? '-translate-x-full' : 'translate-x-0'
+  }`}
+>
+  <div className="flex items-center justify-between p-4 border-b">
+    <h2 className="text-lg font-semibold">Menu</h2>
+    <button onClick={toggleSidebar}>
+      <XIcon className="w-5 h-5" />
+    </button>
+  </div>
+  <ul className="flex flex-col gap-4 p-4">
+    <li>
+      <Link href="/" onClick={toggleSidebar}>Home</Link>
+    </li>
+    <li>
+      <Link href="/about" onClick={toggleSidebar}>About</Link>
+    </li>
+    <li>
+      <Link href="/contact" onClick={toggleSidebar}>Contact</Link>
+    </li>
+  </ul>
+</div>
+
     )
 }
 
