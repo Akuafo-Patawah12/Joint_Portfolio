cb317aa633ea5a8f4481dbedf4b781d3dfca1b19
Akuafo-Patawah12 <burxells873@gmail.com>
2025-06-15 18:40:19 +0000

    update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    esmExternals: true, // Uses ESM if available in node_modules
-  },
-  swcMinify: true, // SWC is fast + efficient
-
+  
 };
 
 export default nextConfig;
