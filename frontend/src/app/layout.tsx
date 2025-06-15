96beddada6a52438a1f1450fdf4bffe140372be5
Akuafo-Patawah12 <burxells873@gmail.com>
2025-06-15 23:59:54 +0000

    update

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import Header from "./(components)/Header"
 import Footer from "./(components)/Footer"
 import NavModal from "./(components)/NavModal"
@@ -91,6 +92,7 @@ export default function RootLayout({
         <Header />
          <NavModal />
         {children}
+        <Analytics />
         <ToastContainer position="top-right" autoClose={3000} />
         <Footer />
         </StoreProvider>
